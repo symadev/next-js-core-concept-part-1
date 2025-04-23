2d96135a4df5846df7520be2aaa69900e7773b72
syma sultana <symasultana2002@gmail.com>
2025-04-23 10:46:23 +0600

    added the basic routing

---
+++ src/app/about/address/page.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function AddressPage() {
+  return (
+    <div className='text-3xl text-amber-300 font-bold'>AddressPage</div>
+  )
+}
---
+++ src/app/about/page.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function AboutPage() {
+  return (
+    <div ><p className='text-3xl font-bold'>aboutSection</p></div>
+  )
+}
